15b537d214f657acad6120181d120df2c7a954fc
ivanolar <olarterodriguezi@vuw.leidenuniv.nl>
2025-12-04 19:52:01 +0100

    changed main

--- main.py
+++ main.py
@@ -32,10 +32,11 @@ runnerOptions = {"open_radioss_main_path":orss_main_path,
 }
 
 def main():
-    sim_id = 269 # Attribute to define the simulation id and connected results folder name
-    vector = np.random.uniform(-5,-4.8,(3,)).tolist()  # Vector where the objective function is evaluated, it has as many components as the second input argument in get_problem below
+    sim_id = 271 # Attribute to define the simulation id and connected results folder name
+    vector = np.random.uniform(-5,5,(5,)).tolist()  # Vector where the objective function is evaluated, it has as many components as the second input argument in get_problem below
     # vector = np.zeros((30,)).tolist()] # Vector where the objective function is evaluated, it has as many components as the second input argument in get_problem below
-    f = sob.get_problem(2,3,runnerOptions,["intrusion","mass","mean_impact_force","max_impact_force","load_uniformity"],sequential_id_numbering=False)
+    print(f"Evaluating vector: {vector}")
+    f = sob.get_problem(1,5,runnerOptions,["intrusion","mass","mean_impact_force","max_impact_force","load_uniformity"],sequential_id_numbering=False)
     obj_value = f(vector,sim_id)
     print(obj_value)
     
